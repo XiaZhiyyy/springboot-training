2dbdffd4342d75754a0769f52941f1e8528c0435
ObserverSum <143597754+ObserverSum@users.noreply.github.com>
2024-11-02 16:11:11 +0800

    创建启动类，测试Tomcat服务器启动是否正常

---
+++ src/main/java/com/hqyj/MyApplication.java
@@ -0,0 +1,19 @@
+/*
+*
+* 时间：2024/11/2 16:08:52
+*
+* 版本：1.0
+* */
+package com.hqyj;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApplication {
+    public static void main(String[] args){
+        // Tomcat服务器启动
+        SpringApplication.run(MyApplication.class,args);
+
+    }
+
+}
